9c1fc258d0725524da759774918d3837b3b2bf72
TommyZ7 <tktomo7260t@gmail.com>
2025-05-17 13:50:54 +0900

    feat: ホームページから不要な要素を削除し、リンクを整理

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,39 @@
 import { Link } from '@heroui/link'
-import { Snippet } from '@heroui/snippet'
-import { Code } from '@heroui/code'
-import { button as buttonStyles } from '@heroui/theme'
-
-import { siteConfig } from '@/config/site'
-import { title, subtitle } from '@/components/primitives'
-import { GithubIcon } from '@/components/icons'
+import { button } from '@heroui/theme'
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: 'violet' })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: 'mt-4' })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: 'primary',
-            radius: 'full',
-            variant: 'shadow',
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: 'bordered', radius: 'full' })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      <Link
+        className={button({
+          color: 'primary',
+          radius: 'full',
+          variant: 'shadow',
+        })}
+        href="/check-list"
+      >
+        Check List
+      </Link>
+      <Link
+        className={button({
+          color: 'primary',
+          radius: 'full',
+          variant: 'shadow',
+        })}
+        href="/create-room"
+      >
+        Create Room
+      </Link>
+      <Link
+        className={button({
+          color: 'primary',
+          radius: 'full',
+          variant: 'shadow',
+        })}
+        href="/show-room"
+      >
+        Show Rooms
+      </Link>
     </section>
   )
 }
